541dc4a091fad5f51150db3f9b91425516edba64
Dominik-Hillmann <dominik.hillmann@gmx.de>
2020-09-04 10:58:40 +0200

    Framing done

--- pin_maps/pin_maps.py
+++ pin_maps/pin_maps.py
@@ -12,7 +12,16 @@ def create_output_dir() -> None:
     working_dir_content = os.listdir(os.getcwd())
     if not 'output' in working_dir_content:
         os.mkdir(os.path.join(os.getcwd(), 'output'))
-    
+   
+
+def frame_img(img: Image, added_frame_px: int) -> Image:
+    width, height = img.size
+    frame_dims = (width + added_frame_px * 2, height + added_frame_px * 2)
+    framed_img = Image.new(img.mode, frame_dims, (255, ) * 3)
+    framed_img.paste(img, (added_frame_px, ) * 2)
+
+    return framed_img
+
 
 def main() -> None:
     create_output_dir()
@@ -21,11 +30,13 @@ def main() -> None:
     print(img.size)
     (left, right, top, bottom) = (300, 1745, 650, 2580)
     img = img.crop((left, top, right, bottom))
+    print(img.__doc__)
+    print(img.crop.__doc__)
     width_cropped, height_cropped = img.size
     print(width_cropped, height_cropped)
 
     dims_new = (width_cropped, height_cropped + 500)
-    img_new = Image.new(img.mode, dims_new, (255, 255, 255))
+    img_new = Image.new(img.mode, dims_new, (255, ) * 3)
     img_new.paste(img, (0, 0))
     
     img_new.save(os.path.join(os.getcwd(), 'output', 'edited.png'))
@@ -33,15 +44,32 @@ def main() -> None:
     text = u'Das ist ein Test äöü!'
 
     font = ImageFont.load_default().font
-    anton_font = ImageFont.truetype('anton.ttf', 120)
+    img_new_width, img_new_height = img_new.size
+    font_size = 500 # Arbitrary start value
+    anton_font = ImageFont.truetype('anton.ttf', font_size)
+    font_width, font_height = anton_font.getsize(text) 
+    
+    print('Img dims:', img_new_width)
+    print(f'Width: {font_width}, Size: {font_size}')
+    while font_width > img_new_width:
+        print(f'Width: {font_width}, Size: {font_size}')
+        font_size -= 1
+        anton_font = ImageFont.truetype('anton.ttf', font_size)
+        font_width, font_height = anton_font.getsize(text)
+
+
     print('Size Anton:', anton_font.getsize(text))
     playfair_font = ImageFont.truetype('playfair.ttf', 120)
     print('playfair size:', playfair_font.getsize(text))
-
+    
+    added_frame_px = 150
     draw = ImageDraw.Draw(img_new)
-    draw.text((200, 1620), text, 'black', playfair_font)
-    img_new.save(os.path.join(os.getcwd(), 'output', 'written.png'))
+    draw.text((0, height_cropped + added_frame_px), text, 'black', anton_font)
 
+    framed_img = frame_img(img_new, added_frame_px)
+    framed_img.save(os.path.join(os.getcwd(), 'output', 'written.png'))
+    
+    # img_new.save(os.path.join(os.getcwd(), 'output', 'written.png'))
 
 if __name__ == '__main__':
     main()
