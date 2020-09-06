d6d34f96caff0d49476a36fde0333c4fa5a5df7e
Dominik-Hillmann <dominik.hillmann@gmx.de>
2020-09-06 12:02:35 +0200

    Completed pattern search

--- pin_maps/pin_maps.py
+++ pin_maps/pin_maps.py
@@ -7,6 +7,9 @@ from copy import deepcopy
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
+# Typing
+from typing import List, Tuple
+
 
 def create_output_dir() -> None:
     working_dir_content = os.listdir(os.getcwd())
@@ -26,41 +29,35 @@ def frame_img(img: Image, added_frame_px: int) -> Image:
 def pattern_2nd_text(
     text: str, 
     img_width: int, 
-    start_height: int, 
+    # start_height: int, 
     font: ImageFont, 
     font_size: int = 80, 
     line_dist: int = 10
-):
-    print(text)
+) -> List[Tuple[int, str]]:
     lines = []
     starts = []
     text = text.split(' ')
 
     while len(text) > 0:
-        # print(text)
         this_line = deepcopy(text)
         line_width, line_height = font.getsize(' '.join(this_line))
         
         while line_width > img_width:
-            # next_line.insert(0, this_line.pop())
             this_line.pop()
-            # print(this_line)
-            # print(text)
             line_width, line_height = font.getsize(' '.join(this_line))
 
-        lines.insert(0, ' '.join(this_line))
-        starts.insert(0, round((img_width - line_width) / 2))
+        lines.append(' '.join(this_line))
+        starts.append(round((img_width - line_width) / 2))
         
         for _ in this_line:
-            # print(text)
             text.pop(0)
 
-    return zip(starts, lines)
+    return list(zip(starts, lines))
 
 
 def main() -> None:
     height_text_space = 700
-    text = u'Das ist ein Test im ÖÖ!'
+    text = u'Inge & Maik-Günter'
     added_frame_px = 150
 
     ### DIR, CROPPING & ERWEITERUNG ###
@@ -78,26 +75,38 @@ def main() -> None:
     ### WRITING FIRST HEADER ###
     img_new_width, img_new_height = img_new.size
     font_size = 500 # Arbitrary start value
-    anton_font = ImageFont.truetype('anton.ttf', font_size)
+    anton_font = ImageFont.truetype('open-sans-bold.ttf', font_size)
     font_width, font_height = anton_font.getsize(text) 
     
     print(f'Width: {font_width}, Size: {font_size}')
     while font_width > img_new_width:
         print(f'Width: {font_width}, Size: {font_size}')
         font_size -= 1
-        anton_font = ImageFont.truetype('anton.ttf', font_size)
+        anton_font = ImageFont.truetype('open-sans-bold.ttf', font_size)
         font_width, font_height = anton_font.getsize(text)
 
     print('Size Anton:', anton_font.getsize(text))
   
     # Add text below using the width of the frame that will be added later on.
+    adjustment = 20 # Number of adjustment pixels to make text look better
     draw = ImageDraw.Draw(img_new)
-    draw.text((0, height_cropped + added_frame_px), text, 'black', anton_font)
-
+    draw.text((0, height_cropped + added_frame_px - adjustment), text, 'black', anton_font)
+    # draw.text((0, height_cropped), text, 'black', anton_font)
 
     ### WRITING SECOND HEADER ###
-    t = 'Das ist ein sehr langer Text. ' * 3 
-    print(list(pattern_2nd_text(t, img_new_width, 100, anton_font)))
+    line_spacing = 30
+
+    t = 'Das ist ein sehr langer Text. ' * 5 
+    hori_start = height_cropped + added_frame_px + font_height + line_spacing # Height of first heading
+
+    font_below = ImageFont.truetype('prata-regular.ttf', 70)
+    font_below_width, font_below_height = font_below.getsize(t)
+    
+    pattern = pattern_2nd_text(t, img_new_width, font_below)
+    for vert_start, line in pattern:
+        draw_pos = (vert_start, hori_start)
+        draw.text(draw_pos, line, 'black', font_below)
+        hori_start += font_below_height + line_spacing
 
 
     ### FRAMING ###
